b1c4a41a0d162ac2bdc766dc67c4b77253cf9939
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-23 20:11:37 +0000

    commincated name with server

--- client.js
+++ client.js
@@ -9,10 +9,17 @@ const connect = function() {
     host: '192.168.88.149',
     port: 50541
   });
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server")
+  })
+  conn.on('connect', () => {
+    conn.write('Name: EKS')
+  })
   // receives data from server
   conn.on('data', (data) => {
     console.log(data)
   })
+ 
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
